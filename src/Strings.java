6322c01198be83aae17125b2cfa5399bb8858eac
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-04-25 14:48:35 +0200

    Zeitmessungen mit dem String und verwandten Klassen.

--- src/Strings.java
+++ src/Strings.java
@@ -1,5 +1,25 @@
 public class Strings {
 
+    public static String array2stringMoreFast(int[] array) {
+        StringBuilder ausgabe = new StringBuilder();
+        for (int i=0; i < array.length; i++) {
+            if (i > 0)
+                ausgabe.append(",");
+            ausgabe.append(array[i]);
+        }
+        return ausgabe.toString();
+    }
+
+    public static String array2stringFast(int[] array) {
+        StringBuffer ausgabe = new StringBuffer();
+        for (int i=0; i < array.length; i++) {
+            if (i > 0)
+                ausgabe.append(",");
+            ausgabe.append(array[i]);
+        }
+        return ausgabe.toString();
+    }
+
     public static String array2string(int[] array) {
         String ausgabe = "";
         for (int i=0; i < array.length; i++) {
@@ -13,14 +33,29 @@ public class Strings {
     }
 
     public static void main(String[] args) {
-        int[] array = new int[1000];
+        int[] array = new int[100000];
 
         // Array mit Zufallszahlen initialisieren
         for (int i=0; i < array.length; i++ ) {
             array[i] = (int)(Math.random() * 100);
         }
 
-        System.out.println( array2string(array) );
+        long laufzeit = System.nanoTime();
+        array2string(array);
+        laufzeit = System.nanoTime() - laufzeit;
+        System.out.println(laufzeit/1000 + "µs");
+
+        laufzeit = System.nanoTime();
+        array2stringFast(array);
+        laufzeit = System.nanoTime() - laufzeit;
+        System.out.println(laufzeit/1000 + "µs");
+
+        laufzeit = System.nanoTime();
+        array2stringMoreFast(array);
+        laufzeit = System.nanoTime() - laufzeit;
+        System.out.println(laufzeit/1000 + "µs");
+
+        String string = "Hallo Text";
     }
 
 }
