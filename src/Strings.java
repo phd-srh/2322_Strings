99a5608e00de903ebfb307adeb3aecec5a9cac43
Dr. Dillinger <peter.dillinger@srh.de>
2023-04-25 13:53:00 +0200

    Erste naive Methode, um ein Array von Zahlen auszugeben.

--- src/Strings.java
+++ src/Strings.java
@@ -1,2 +1,26 @@
 public class Strings {
+
+    public static String array2string(int[] array) {
+        String ausgabe = "";
+        for (int i=0; i < array.length; i++) {
+            if (i > 0)
+                ausgabe = ausgabe + ",";
+            ausgabe = ausgabe + array[i];
+//            if (i < array.length-1)
+//                ausgabe = ausgabe + ",";
+        }
+        return ausgabe;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[1000];
+
+        // Array mit Zufallszahlen initialisieren
+        for (int i=0; i < array.length; i++ ) {
+            array[i] = (int)(Math.random() * 100);
+        }
+
+        System.out.println( array2string(array) );
+    }
+
 }
