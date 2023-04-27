1ccde7a64d41eeb6997580597ca3c432a139e1c7
Dr. Dillinger <peter.dillinger@srh.de>
2023-04-27 14:02:44 +0200

    Keine Ahnung, was wir da gemacht haben...

--- src/AutoMain.java
+++ src/AutoMain.java
@@ -1,8 +1,23 @@
 public class AutoMain {
     public static void main(String[] args) {
-        PKW opel = new PKW(2, "Benzin", 50, 4);
-        PKW vw = new PKW(2, "Elektro", 35, 5);
-        PKW audi = new PKW(2, "Diesel", 42, 3);
+        PKW opel  = new PKW(2, "Benzin", 50, 4);
+        PKW vw    = new PKW(2, "Elektro", 35, 5);
+        Auto audi = new PKW(2, "Diesel", 42, 3);
+        //new LKW(3, "Diesel", true, 2.5);
+
+        int   s = 170_000;
+        short x = (short)s;
+        System.out.println(x);
+
+        if (audi instanceof Object) {
+            System.out.println("Der Audi ist ein Object");
+        }
+        if (audi instanceof PKW){
+            ((PKW) audi).getKofferraum();
+        }
+        else if (audi instanceof LKW) {
+            ((LKW) audi).isAnhänger();
+        }
 
         Garage garage = new Garage(2);
         System.out.println( garage.autoParken(opel) );
